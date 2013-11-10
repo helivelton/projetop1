1cc44b2b410b1cfef5cf491a3ecbf87af97651b7
carlosmaster10@gmail.com <carlosmaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-11-10 17:50:41 +0000

    -Criação do arquivo basico.c, com as funções básicas. -Processo de referencia de imagem melhorado (por meio de função) -Criação do arquivo basico.h, com a explicação das funções de basico.c

---
+++ trunk/Projeto P1/basico.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <allegro.h>
+#include <string.h>
+
+#define LINKRELAT "../../Dropbox/"
+
+void init()
+{
+    int res;
+    allegro_init();
+
+    set_color_depth(32);
+    res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640 , 480 , 0 , 0 ); // cria tela 640x480
+
+    if (res != 0 ) // verifica se houve erro na criação da tela
+    {
+        allegro_message(allegro_error);
+        exit( -1 );
+    }
+    // instalação do timer, teclado e mouse
+    install_timer();
+    install_keyboard();
+    install_mouse();
+}
+
+void deinit()
+{
+    clear_keybuf(); // libera espaço
+}
+
+char *link_imagem(char caminho[256])
+{
+    char adicao[256]=LINKRELAT;
+    strcat(adicao,caminho);
+    return adicao;
+}
