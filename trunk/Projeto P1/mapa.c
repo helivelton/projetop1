8d7575cac9e22cb5e01b1ddeb65a233d4875b29f
CarlosMaster10@gmail.com <CarlosMaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-13 01:43:44 +0000

    -revertendo para a revisão 30

--- trunk/Projeto P1/mapa.c
+++ trunk/Projeto P1/mapa.c
@@ -7,9 +7,9 @@ void carrega_texturas(BITMAP *texturas[MAX_TERRENOS])
     {
         texturas[i] = create_bitmap(32,32);
     }
-    BITMAP *tileset=load_bitmap(link_imagem("imagens_p1/tiles01b.bmp"),NULL);// tileset b·sico
+    BITMAP *tileset=load_bitmap(link_imagem("imagens_p1/tiles01b.bmp"),NULL);// tileset b√°sico
     blit(tileset,texturas[TERRA], 0*32, 0*32,0,0,32,32); //terra
-    blit(tileset,texturas[CHAO], 1*32, 0*32,0,0,32,32); //ch„o
+    blit(tileset,texturas[CHAO], 1*32, 0*32,0,0,32,32); //ch√£o
     blit(tileset,texturas[PEDRA], 0*32, 1*32,0,0,32,32); //pedra
     blit(tileset,texturas[ARVORE_MORTA], 8*32, 8*32,0,0,32,32); //arvore morta
     blit(tileset,texturas[ARVORE_1], 6*32,10*32,0,0,32,32); //arvore parte 1
@@ -24,51 +24,71 @@ void carrega_texturas(BITMAP *texturas[MAX_TERRENOS])
     destroy_bitmap(tileset);
 }
 
-void prepara_mapa(int matriz_tela[ALTURA_MAPA/32][LARGURA_MAPA/32])
+void prepara_mapa(int matriz_tela[ALTURA_MAPA/32][LARGURA_MAPA/32],char nome_mapa[10])
 {
-    int n_matriz_tela[15][24] =
-    //   0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23
+    int i=0,j=0,m;
+    char carac;
+    FILE* f = fopen(nome_mapa, "r");
+    while((carac = getc(f)) != EOF)
     {
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 0
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 1
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 2
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 3
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 4
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 5
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 6
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 7
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 8
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 9
-        {-1,-1,-1,-1,-1,-1,-1, 4, 5, 6,-1,-1, 4, 5, 6,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 10
-        {-1,-1,-1,-1,-1,-1,-1, 7, 8, 9,-1,-1, 7, 8, 9,-1,-1,-1,-1,-1,-1,-1,-1,-1}, // 11
-        {-1,-1,-1,-1,-1, 3,-1,10,11,12,-1,-1,10,11,12,-1,-1,-1, 2,-1,-1,-1,-1,-1}, // 12
-        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 13
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  // 14
-    };
-    int i,j;
-    for(i=0;i<15;i++)
-    {
-        for(j=0;j<24;j++)
+        // carrega tiles
+        if(j<LARGURA_MAPA/32)
         {
-            matriz_tela[i][j]=n_matriz_tela[i][j];
+            switch(carac)
+            {
+                case '0':
+                    m=0;
+                    break;
+                case '1':
+                    m=1;
+                    break;
+                case '2':
+                    m=2;
+                    break;
+                case '3':
+                    m=3;
+                    break;
+                case '4':
+                    m=4;
+                    break;
+                case '5':
+                    m=5;
+                    break;
+                case '6':
+                    m=6;
+                    break;
+                case '7':
+                    m=7;
+                    break;
+                case '8':
+                    m=8;
+                    break;
+                case '9':
+                    m=9;
+                    break;
+                case 'a':
+                    m=10;
+                    break;
+                case 'b':
+                    m=11;
+                    break;
+                case 'c':
+                    m=12;
+                    break;
+                default:
+                    m=-1;
+            }
+            matriz_tela[i][j]=m;
+            j++;
+        }
+        else
+        {
+            i++;
+            j=0;
         }
     }
-    for(i=13;i<15;i++)
-    {
-        for(j=24;j<LARGURA_MAPA/32;j++)
-        {
-            if(i==13)
-                matriz_tela[i][j]=1;
-            if(i==14)
-                matriz_tela[i][j]=0;
-        }
-    }
-    for(i=0;i<13;i++)
-    {
-        for(j=24;j<LARGURA_MAPA/32;j++)
-            matriz_tela[i][j]=-1;
-    }
-    matriz_tela[12][LARGURA_MAPA/32-3]=3;
+
+    fclose(f);
 }
 
 void carrega_mapa(BITMAP *mapa,BITMAP *texturas[MAX_TERRENOS],int matriz_tela[ALTURA_MAPA/32][LARGURA_MAPA/32])
