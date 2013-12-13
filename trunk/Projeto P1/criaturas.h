991df218502006a132a0f763054663b329187f8d
CarlosMaster10@gmail.com <CarlosMaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-13 01:49:17 +0000

    -revertendo para a revisão 30

--- trunk/Projeto P1/criaturas.h
+++ trunk/Projeto P1/criaturas.h
@@ -4,17 +4,47 @@
 #include "basico.h"
 #include "controle.h"
 
-// Funções para todas as criaturas
+typedef struct atributo
+{
+    int forca;
+    int habilidade;
+    int resistencia;
+    int armadura;
+    int poder_de_fogo;
+    int hp;
+    int mp;
+}Tatributo;
+
+typedef struct criatura
+{
+    float x;
+    float y;
+    float largura;
+    float altura;
+    int direcao;
+    int direcao_anterior;
+    int estado_sprite;
+    int controle_estado;
+    int caindo;
+    int pulando;
+    BITMAP *sprite;
+    BITMAP *vetor_sprite[4];
+    Tatributo caracteristicas;
+}Tcriatura;
+
+// FunÃ§Ãµes para todas as criaturas
 void preenche_criatura(Tcriatura *ser,float x,float y,float largura, float altura,int direcao,int f,int h,int r,int a,int pdf);
 
-// Funções do guerreiro
-void imagens_guerreiro(Tcriatura *guerreiro);
+// FunÃ§Ãµes do guerreiro
+void imagens_guerreiro(BITMAP *im_guerreiro[4]);
 void movimento_guerreiro(Tcriatura *guerreiro,int mov_mapa[2]);
 void desenhar_guerreiro(BITMAP *buffer,Tcriatura *guerreiro);
 
-// Funções do goblin tipo 1
+// FunÃ§Ãµes do goblin tipo 1
 void imagens_goblin1(BITMAP *im_goblin1[4]);
 void movimento_goblin1(Tcriatura *goblin1,int x_guerreiro);
 void desenhar_goblin1(BITMAP *buffer,Tcriatura *goblin1);
+
+int colisao(float ax,float ay, float ah, float al, float bx, float by, float bh, float bl);
 
 #endif // CRIATURAS_H
