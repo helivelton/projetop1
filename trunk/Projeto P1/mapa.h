4282d182974277cf3b2dfdabab2df6629426e682
CarlosMaster10@gmail.com <CarlosMaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-13 01:46:19 +0000

    -revertendo para a revisão 30

--- trunk/Projeto P1/mapa.h
+++ trunk/Projeto P1/mapa.h
@@ -22,14 +22,14 @@ MAX_TERRENOS
 
 /*
 Prepara as texturas individuais.
-Só é chamada uma vez no jogo.
+SÃ³ Ã© chamada uma vez no jogo.
 */
 void carrega_texturas(BITMAP *texturas[MAX_TERRENOS]);
 
 /*
 Prepara a matriz do mapa
 */
-void prepara_mapa(int matriz_tela[SCREEN_H/32][SCREEN_W/32]);
+void prepara_mapa(int matriz_tela[ALTURA_MAPA/32][LARGURA_MAPA/32],char nome_mapa[10]);
 
 /*
 Desenha o mapa.
