02642d49eb59272fa16314a65e305f8cb743dc16
carlosmaster10@gmail.com <carlosmaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-20 22:39:51 +0000

    -Criação de poção de cura e desenho na tela

--- Projeto P1/item.c
+++ Projeto P1/item.c
@@ -1,10 +1,18 @@
 #include "item.h"
 
-void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255])
+void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255],int posicaox,int posicaoy)
 {
-    objeto->imagem=load_bitmap(link_imagem(imagem_link),NULL);
+    BITMAP *tiles = load_bitmap(link_imagem(imagem_link),NULL);
+    objeto->imagem = create_bitmap(32,32);
+    blit(tiles,objeto->imagem,posicaox*32,posicaoy*32,0,0,32,32);
     objeto->x=x;
     objeto->y=y;
     objeto->altura=altura;
     objeto->largura=largura;
+    destroy_bitmap(tiles);
+}
+
+void desenhar_item(BITMAP *buffer,Titem *objeto,int mov_mapa[2])
+{
+    draw_sprite(buffer, objeto->imagem, objeto->x + mov_mapa[0],objeto->y-objeto->altura); // manda objeto para buffer
 }
--- Projeto P1/item.h
+++ Projeto P1/item.h
@@ -3,6 +3,7 @@
 
 #include "basico.h"
 
-void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255]);
+void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255],int posicaox,int posicaoy);
+void desenhar_item(BITMAP *buffer,Titem *objeto,int mov_mapa[2]);
 
 #endif // ITEM_H
