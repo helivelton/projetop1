a9ccb05bd1205c807b186aa02a2e8ef5e648808c
carlosmaster10@gmail.com <carlosmaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-24 15:44:48 +0000

    -Função de ataque já está funcionando, mas com a tecla Z (espaço por algum motivo não aceitava LEFT+SPACE+UP)

--- Projeto P1/criaturas.h
+++ Projeto P1/criaturas.h
@@ -16,12 +16,12 @@ void calcular_dano(Tcriatura* atacante, Tcriatura* alvo,int tipo_ataque);
 // Funções do guerreiro
 void imagens_guerreiro(Tcriatura *guerreiro);
 void movimento_guerreiro(Tcriatura *guerreiro,int mov_mapa[2], int matriz_tela[ALTURA_MAPA/32][LARGURA_MAPA/32], int bloqueios[3]);
-void ataque_guerreiro(Tcriatura *guerreiro,int tempo_jogo);
+void ataque_guerreiro(Tcriatura *guerreiro,int tempo_jogo,Toponentes *inimigos, int mov_mapa[2]);
 void desenhar_guerreiro(BITMAP *buffer,Tcriatura *guerreiro);
 
 // Funções do goblin tipo 1
 void imagens_goblin1(Tcriatura *goblin1);
-void movimento_goblin1(Tcriatura *goblin1,int x_guerreiro);
+void movimento_goblin1(Tcriatura *goblin1,int x_guerreiro,int tempo_jogo);
 void desenhar_goblin1(BITMAP *buffer,Tcriatura *goblin1);
 
 #endif // CRIATURAS_H
