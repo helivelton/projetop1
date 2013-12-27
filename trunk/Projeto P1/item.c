f3ee2979d9b1b80a43b080b7941921a19ad942ce
carlosmaster10@gmail.com <carlosmaster10@gmail.com@00fdaa98-9aae-cb95-bd65-91a21b3fb303>
2013-12-27 17:31:39 +0000

    -preenchimento das variáveis da fase em uma função do módulo de eventos.

--- trunk/Projeto P1/item.c
+++ trunk/Projeto P1/item.c
@@ -1,18 +1,25 @@
 #include "item.h"
 
 void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255],int posicaox,int posicaoy,
-                    int ativo, int tipo)
+                    int ativo, int tipo,int imagem_preenchida)
 {
     BITMAP *tiles = load_bitmap(link_imagem(imagem_link),NULL);
-    objeto->imagem = create_bitmap(32,32);
+
+    if(!imagem_preenchida)
+        objeto->imagem = create_bitmap(32,32);
+    else
+        clear_bitmap(objeto->imagem);
+
     blit(tiles,objeto->imagem,posicaox*32,posicaoy*32,0,0,32,32);
+
     objeto->x=x;
     objeto->y=y;
     objeto->altura=altura;
     objeto->largura=largura;
-    destroy_bitmap(tiles);
     objeto->tipo=tipo;
     objeto->ativo=ativo;
+
+    destroy_bitmap(tiles);
 }
 
 void desenhar_item(BITMAP *buffer,Titem *objeto,int ajuste_x)
--- trunk/Projeto P1/item.h
+++ trunk/Projeto P1/item.h
@@ -4,7 +4,7 @@
 #include "basico.h"
 
 void preencher_item(Titem *objeto, float x, float y, int altura, int largura, char imagem_link[255],int posicaox,int posicaoy,
-                    int ativo, int tipo);
+                    int ativo, int tipo,int imagem_preenchida);
 void desenhar_item(BITMAP *buffer,Titem *objeto,int ajuste_x);
 void verifique_efeito(Titens *itens,Tcriatura *guerreiro);
 
